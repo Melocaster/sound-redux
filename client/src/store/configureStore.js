a3bdf2b19200eaecbf09c9c9bb1d9df2a9cbed3c
Melocaster <wenhanshen92@gmail.com>
2017-12-19 14:47:45 -0700

    Add devTool extension

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,11 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = createStoreWithMiddleware(rootReducer, initialState, enhancers);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {

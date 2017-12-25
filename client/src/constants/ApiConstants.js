5f66100afd6e6dbed9be002b124e5fc80d9e8482
Melocaster <wenhanshen92@gmail.com>
2017-12-24 18:46:28 -0700

    Fix a new soundcloud API id

--- client/src/constants/ApiConstants.js
+++ client/src/constants/ApiConstants.js
@@ -1,5 +1,5 @@
 const API_HOSTNAME = '//api.soundcloud.com';
-export const CLIENT_ID = '89ae6050d2fb321d2ac9be2e2d822596';
+export const CLIENT_ID = 'a0f84e7c2d612d845125fb5eebff5b37';
 
 const constructUrl = url => `${API_HOSTNAME}${url}${url.indexOf('?') === -1 ? '?' : '&'}client_id=${CLIENT_ID}`;
 
